b15aa70bc1efaef130d34217e7700ec1a0d56b2e
tealhyperion <a46114316@gmail.com>
2022-12-19 20:41:29 +0900

    n6

--- Assets/02.Script/Sound.cs
+++ Assets/02.Script/Sound.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sound : MonoBehaviour
 {
-    public AudioSource musicsource;
+
+    public Slider SFXSource;
+    public Slider MasterSource;
+    public Slider MusicSource;
+
+    public AudioSource Musicsource;
+    public AudioSource SFXsource;
 
     public void SetMusicVolume(float volume)
     {
-        musicsource.volume = volume;
+        Musicsource.volume = volume * MasterSource.value;
+    }
+
+
+    public void SetSFXVolume(float volume)
+    {
+        SFXsource.volume = volume * MasterSource.value;
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        SFXsource.volume = SFXSource.value * volume;
+        Musicsource.volume = MusicSource.value * volume;
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
     }
 }
