8afaaaba4e63eead1a61aab91a220d399c8004c8
tealhyperion <a46114316@gmail.com>
2022-12-20 10:29:17 +0900

    n

--- Assets/02.Script/TP/TP.cs
+++ Assets/02.Script/TP/TP.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TP : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject tp1;
+    public string tp2;
 
-    // Update is called once per frame
-    void Update()
+    //public void ChangeScene()
+    //{
+
+    //}
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Z))   //만약 목적지에서 z를 누르면
+        {
+            Debug.Log("눌림");
+            SceneManager.LoadScene(tp2);    //다음씬으로 이동
+        }
     }
 }
