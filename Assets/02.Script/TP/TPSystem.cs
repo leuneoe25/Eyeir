300a7fb10c2a90c9fcdc2f5412b26ab2802abb3a
tealhyperion <a46114316@gmail.com>
2022-12-20 14:08:18 +0900

    n

--- Assets/02.Script/TP/TPSystem.cs
+++ Assets/02.Script/TP/TPSystem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class TPSystem : MonoBehaviour
 {
     #region Singleton
@@ -36,8 +36,11 @@ public class TPSystem : MonoBehaviour
     private Vector2 pos;
     public GameObject Player;
 
+    private bool state;
+
     private void Start()
     {
+        state = true;
         nowScene = "";  //초기값
         PlayScene = SceneManager.GetActiveScene().name;
         Debug.Log("");
@@ -46,14 +49,19 @@ public class TPSystem : MonoBehaviour
     {
         if(SceneManager.GetActiveScene().name != PlayScene)    //플레이중인 씬과 초기씬이 다르다면
         {
-
+            gameObject.SetActive(false);
+            state = false;
             Instantiate(Player, pos, Quaternion.identity);//위치를 이동시킨다.
             PlayScene = SceneManager.GetActiveScene().name;     //초기씬을 현재씬으로 덮는다.
         }
     }
     public void TP(string tp2, Vector2 Position)
     {
-        //pos = Position;
+        pos = Position;
         SceneManager.LoadScene(tp2);        //씬 이동
+        gameObject.SetActive(true);
+        state = true;
     }
+
+    
 }
