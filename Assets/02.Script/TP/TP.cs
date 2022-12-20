9c352b94888f55d8f5c3cbfd5f1bce6d565602f1
tealhyperion <a46114316@gmail.com>
2022-12-20 11:26:28 +0900

    n

--- Assets/02.Script/TP/TP.cs
+++ Assets/02.Script/TP/TP.cs
@@ -7,17 +7,19 @@ public class TP : MonoBehaviour
 {
     public GameObject tp1;
     public string tp2;
+    public Vector2 Position;
 
-    //public void ChangeScene()
-    //{
-
-    //}
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Z))   //만약 목적지에서 z를 누르면
         {
             Debug.Log("눌림");
-            SceneManager.LoadScene(tp2);    //다음씬으로 이동
+            TPSystem.Instance.sa(tp2, Position);    //다음씬으로 이동
+
         }
+        
     }
+
+
+
 }
